f66717b2575292445c088d7c8b7d396b3eeecf6b
ianalejandrook14x <taiwam34@gmail.com>
2024-09-30 13:33:00 -0300

    Update serbot-bots.js

--- plugins/serbot-bots.js
+++ plugins/serbot-bots.js
@@ -8,12 +8,12 @@ global.conns.forEach((conn) => {
 if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
 uniqueUsers.set(conn.user.jid.replace(/[^0-9]/g, ''), conn.user)}})
 
-const message = Array.from(uniqueUsers.values()).map((user, index) => `┌  ✩  *${index + 1}* : @${user.jid.replace(/[^0-9]/g, '')}\n│  ✩  *Link* : http://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n└  ✩  *Nombre* : ${user.name || '▰▱▰▱▰▱▰▱▰▱▰▱▰▱·̩̩̥͙＊*•̩̩͙✩•̩̩͙*˚˚*•̩̩͙✩•̩̩͙*˚＊·̩̩̥͙𝑵𝒂𝒌𝒂𝒏𝒐·̩̩̥͙＊*•̩̩͙✩•̩̩͙*˚˚*•̩̩͙✩•̩̩͙*˚＊·̩̩̥͙▰▱▰▱▰▱▰▱▰▱▰▱▰▱'}\n`
+const message = Array.from(uniqueUsers.values()).map((user, index) => `┌  ✩  *${index + 1}* : @${user.jid.replace(/[^0-9]/g, '')}\n│  ✩  *Link* : http://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n└  ✩  *Nombre* : ${user.name || 'ⲯ﹍︿﹍︿﹍ 𝙵𝚄𝚃𝙰𝚁𝙾 ﹍ⲯ﹍ⲯ﹍︿﹍☼'}\n`
   ).join('\n')
   
 const replyMessage = message.length === 0 ? "" : message
 const totalUsers = uniqueUsers.size;
-const responseMessage = `${` –   ▰▱▰▱▰▱▰▱▰▱▰▱▰▱       ❀ ✿ ღ𝑩𝒐𝒕𝒔ღ ❀ ✿    ☆。*。☆。。* 。☆*\n·̩̩̥͙＊*•̩̩͙✩•̩̩͙*˚˚*•̩̩͙✩•̩̩͙*˚＊·̩̩̥͙▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n${replyMessage.trim()}`.trim()}`
+const responseMessage = `${`♥ﮩ٨ـﮩﮩ٨ـﮩﮩ в  σ т ѕ ﮩﮩـ٨ﮩﮩـ٨ﮩ♥\n${replyMessage.trim()}`.trim()}`
   
 let img = await (await fetch(`https://i.ibb.co/cvmV7CV/file.jpg`)).buffer()
 
