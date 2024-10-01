5390aa3523e69ae419604c82a222820d4effc05f
ianalejandrook14x <taiwam34@gmail.com>
2024-10-01 15:28:39 -0300

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Ian;;\nFN:Ian⁩\nORG:Ian⁩\nTITLE:\nitem1.TEL;waid=5493876639332:5493876639332\nitem1.X-ABLabel:\nX-WA-BIZ-DESCRIPTION:Hσʅα ɱҽ ʅʅαɱσ Iαɳ ყ ʂσყ ԃυҽñσ ԃҽ ʅσʂ ρɾσყҽƈƚσʂ Fυƚαɾσ-Bσƚ - IαɳBσƚ-MD, ƚαɱႦιéɳ ʂσყ Mσԃҽɾαԃσɾ ԃҽ CσɾιɳPʅυʂ-Hσʂƚ 🌙\nX-WA-BIZ-NAME:Ian\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;ιαɳαʅҽʝαɳԃɾσσƙ15x;;\nFN:ιαɳαʅҽʝαɳԃɾσσƙ15x⁩\nORG:ιαɳαʅҽʝαɳԃɾσσƙ15x\nTITLE:\nitem1.TEL;waid=5493876639332:5493876639332\nitem1.X-ABLabel:\nX-WA-BIZ-DESCRIPTION:Hσʅα ɱҽ ʅʅαɱσ Iαɳ ყ ʂσყ ԃυҽñσ ԃҽ ʅσʂ ρɾσყҽƈƚσʂ Fυƚαɾσ-Bσƚ - IαɳBσƚ-MD, ƚαɱႦιéɳ ʂσყ Mσԃҽɾαԃσɾ ԃҽ CσɾιɳPʅυʂ-Hσʂƚ 🌙\nX-WA-BIZ-NAME:Ian\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: 'Ian⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
