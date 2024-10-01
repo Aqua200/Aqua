e29ef2b2397df0fe75f81f9b663f0c610122ad59
ianalejandrook14x <taiwam34@gmail.com>
2024-10-01 20:37:34 -0300

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -19,3 +19,10 @@ if (chat.isBaneed) return
 if (/^ian$/i.test(m.text)) {
 conn.reply(m.chat, `☁ Hσʅα, Iαɳ ҽʂ ɱι ƈɾҽαԃσɾ ʂι ϙυιҽɾҽʂ ԋαზʅαɾ ƈσɳ ҽʅ ҽʂƚҽ ҽʂ ʂυ ɳυɱҽɾσ\n\n*+5493876639332*`, m, rcanal, )
 }
+
+  if (/^menu2$/i.test(m.text)) {
+conn.reply(m.chat, `*Hσʅα, ҽʅ ɱҽɳυ2 ƚσԃαʋια ʂιɠυҽ ҽɳ ԃҽʂαɾɾσʅʅσ ρσɾ ϝαʋσɾ ҽʂρҽɾҽ ραƈιҽɳƚҽɱҽɳƚҽ ɱιҽɳƚɾαʂ ʂҽ ʅσ αƈƚυαʅιȥα*\n\n> Aƚƚ ιαɳαʅҽʝαɳԃɾσσƙ15x`, m, rcanal, )
+}
+return !0;
+};
+export default handler;
