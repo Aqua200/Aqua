b2168194a14acee4413030b716e1657f6c1c9ec5
ianalejandrook14x <taiwam34@gmail.com>
2024-10-01 15:00:11 -0300

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -8,17 +8,17 @@ export async function before(m, {conn, participants, groupMetadata}) {
   let chat = global.db.data.chats[m.chat]
 
   if (chat.bienvenida && m.messageStubType == 27) {
-    let bienvenida = `╰⊱🌸≺ *${botname}* \n「 Bιҽɳʋҽɳιԃσ 」\n「  @${m.messageStubParameters[0].split`@`[0]}  」\n 「 ${groupMetadata.subject} 」\n「 Dιʂϝɾυƚҽ ʂҽ ҽʂƚαԃια 」`
+    let bienvenida = `╰⊱🌸≺ *${botname}* \n「 Bιҽɳʋҽɳιԃσ 」\n「  @${m.messageStubParameters[0].split`@`[0]}  」\n 「 ${groupMetadata.subject} 」\n「 Dιʂϝɾυƚҽ ʂҽ ҽʂƚαԃια 🌸 」`
     
 await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
   }
   
   if (chat.bienvenida && m.messageStubType == 28) {
-    let bye = `╰⊱🌸≺ *${botname}* \n「 Hαʂƚα ʅυҽɠσ 」\n「  @${m.messageStubParameters[0].split`@`[0]}  」\n「 ${groupMetadata.subject} 」\n「 Vυҽʅʋҽ ρɾσɳƚσ 」`
+    let bye = `╰⊱🌸≺ *${botname}* \n「 Hαʂƚα ʅυҽɠσ 」\n「  @${m.messageStubParameters[0].split`@`[0]}  」\n「 ${groupMetadata.subject} 」\n「 Vυҽʅʋҽ ρɾσɳƚσ 🌸 」`
 await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo)
   }
   
   if (chat.bienvenida && m.messageStubType == 32) {
-    let kick = `╰⊱🌸≺ *${botname}* \n「 Sҽ ҽʅιɱιɳσ 」\n「  @${m.messageStubParameters[0].split`@`[0]}  」\n「 ${groupMetadata.subject} 」\n「 Aԃισʂ 」`
+    let kick = `╰⊱🌸≺ *${botname}* \n「 Sҽ ҽʅιɱιɳσ 」\n「  @${m.messageStubParameters[0].split`@`[0]}  」\n「 ${groupMetadata.subject} 」\n「 Aԃισʂ 🌸 」`
 await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo)
 }}
