d9b6fca2acfbbda4be7b25175078b357151a71c4
ianalejandrook14x <taiwam34@gmail.com>
2024-10-02 14:41:16 -0300

    Tҽαɱ Sԋιɳιɳɠ Mσσɳ 🌙

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 //import moment from 'moment-timezone' 
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─* 
 
 global.owner = [
   ['5493876639332', 'ιαɳαʅҽʝαɳԃɾσσƙ15x', true],
