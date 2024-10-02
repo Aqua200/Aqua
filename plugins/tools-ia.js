16bd1cc08a21caeea5ab7d3d1effe5b692a02eae
ianalejandrook14x <taiwam34@gmail.com>
2024-10-01 21:25:26 -0300

    Update tools-ia.js

--- plugins/tools-ia.js
+++ plugins/tools-ia.js
@@ -10,7 +10,8 @@ let userid = conn.getName(m.sender) || 'default';
 let apiurl = `https://api.guruapi.tech/ai/gpt4?username=${userid}&query=hii${encodeURIComponent(text)}`;
 let result = await fetch(apiurl);
 let response = await result.json();
-
+const basePrompt = `Tu nombre es Futaro y parece haber sido creado por ianalejandrook15x. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertido, y te encanta aprender. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+  
 await conn.relayMessage(m.chat, { protocolMessage: { key: msg.key, type: 14, editedMessage: { conversation: response.msg }}}, {});
 } catch {}}
 
